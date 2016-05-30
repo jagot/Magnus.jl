46fb439bab52dd2551ef2635a5f15169c6aa13c1
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-05-30 15:32:37 +0200

    Added commutator-free propagator

--- examples/particle_in_box.jl
+++ examples/particle_in_box.jl
@@ -26,7 +26,7 @@ function propagate{T<:AbstractFloat}(ψ₀::AbstractVector{Complex{T}},
                                      tmax::Number, N::Integer,
                                      propagator::MagnusPropagator,
                                      save::Bool = false)
-    τ = tmax/(N+1)
+    τ = tmax/N
     Ψ = save ? Matrix{Complex{T}}(length(ψ₀),N+1) : Vector{Complex{T}}(length(ψ₀))
     Ψ[:,1] = ψ₀
     cur = 1
@@ -39,6 +39,44 @@ function propagate{T<:AbstractFloat}(ψ₀::AbstractVector{Complex{T}},
     Ψ
 end
 
+function test_propagation(name, psi0, tmax, period, N, propagator)
+    @time psi = propagate(psi0,
+                          tmax*period, N,
+                          propagator,
+                          true)
+    t = linspace(0,tmax,N+1)
+    n = Vector{Float64}(length(t))
+    p = Vector{Complex128}(length(t))
+    for i = eachindex(t)
+        n[i] = norm(psi[:,i])
+        p[i] = dot(psi0,psi[:,i])
+        psi[:,i] -= p[i]*psi0
+    end
+
+    figure(name,figsize=(8,10))
+    clf()
+    subplot(311)
+    pcolormesh(t, x, abs2(psi), rasterized=true, cmap = plt[:cm][:get_cmap]("viridis"))
+    margins(0,0)
+    gca()[:set_xticklabels]([])
+    title(L"|\Psi-\Psi_0\langle\Psi_0|\Psi\rangle|^2")
+    ylabel(L"x")
+    subplot(312)
+    plot(t,field(t))
+    margins(0,0.1)
+    gca()[:set_xticklabels]([])
+    ylabel(L"F(t)")
+    subplot(313)
+    plot(t,n, "-")
+    margins(0,0.1)
+    xlabel(L"t/T")
+    ylabel(L"|\Psi|^2")
+    a2 = gca()[:twinx]()
+    a2[:plot](t,abs(p), "--")
+    ylabel(L"|\langle\Psi_0|\Psi\rangle|")
+    tight_layout()
+end
+
 M = 50
 T,ei,x = particle_in_box(M)
 psi0,E0 = state(ei, [1])
@@ -48,49 +86,17 @@ F₀ = 1e4
 period = 1.0/abs(ei[:values][1]-ei[:values][2])
 ω = 2π/4
 mx = Diagonal(collect(x-mean(x)))
+D = LinearOperator(mx, symmetric=true, hermitian=true)
 tmax = 50
-ndt = 300
-dt = 6
-F = t -> F₀*exp(-(t-tmax/2).^2/2dt^2).*sin(ω*t)
-Hᵢ = t -> F(t*period)*LinearOperator(mx, symmetric=true, hermitian=true)
+field = t -> F₀*exp(-(t-tmax/2).^2/2dt^2).*sin(ω*t)
+F = t -> field(t*period)
+Hᵢ = t -> F(t)*D
 H = t -> H₀ + Hᵢ(t)
 
+ndt = 300
+dt = 6
 
-propagator = MidpointPropagator(H, -im, LanczosExponentiator(30,psi0))
-@time psi = propagate(psi0,
-                      tmax*period, tmax*ndt,
-                      propagator,
-                      true)
-
-t = linspace(0,tmax,tmax*ndt+1)
-n = Vector{Float64}(length(t))
-p = Vector{Complex128}(length(t))
-for i = eachindex(t)
-    n[i] = norm(psi[:,i])
-    p[i] = dot(psi0,psi[:,i])
-    psi[:,i] -= p[i]*psi0
-end
-
-figure(1,figsize=(8,10))
-clf()
-subplot(311)
-pcolormesh(t, x, abs2(psi), rasterized=true, cmap = plt[:cm][:get_cmap]("viridis"))
-margins(0,0)
-gca()[:set_xticklabels]([])
-title(L"|\Psi-\Psi_0\langle\Psi_0|\Psi\rangle|^2")
-ylabel(L"x")
-subplot(312)
-plot(t,F(t))
-margins(0,0.1)
-gca()[:set_xticklabels]([])
-ylabel(L"F(t)")
-subplot(313)
-plot(t,n, "-")
-margins(0,0.1)
-xlabel(L"t/T")
-ylabel(L"|\Psi|^2")
-a2 = gca()[:twinx]()
-a2[:plot](t,abs(p), "--")
-ylabel(L"|\langle\Psi_0|\Psi\rangle|")
-tight_layout()
-show()
+test_propagation("midpoint", psi0, tmax, period, tmax*300,
+                 MidpointPropagator(H, -im, LanczosExponentiator(30,psi0)))
+test_propagation("CFET", psi0, tmax, period, tmax*30,
+                 CFET4BfCPropagator(H₀, F, D, -im, LanczosExponentiator(30,psi0)))
--- src/propagators.jl
+++ src/propagators.jl
@@ -17,5 +17,53 @@ call{E<:Exponentiator}(p::MidpointPropagator{E},
                        v::StridedArray, w::StridedArray) =
                            p.Exp(p.A(t+τ/2), p.a*τ, v, w)
 
+#=
 
-export MagnusPropagator, MidpointPropagator
+Implementation of commutator-free exponential time propagators, as
+described in
+
+  Alvermann, A., Fehske, H., & Littlewood, P. B. (2012). Numerical
+  time propagation of quantum systems in radiation fields. New
+  J. Phys., 14(10),
+  105008. http://dx.doi.org/10.1088/1367-2630/14/10/105008
+
+=#
+type CFET4BfCPropagator{E<:Exponentiator} <: MagnusPropagator{E}
+    B::LinearOperator
+    f::Function
+    C::LinearOperator
+    a::Number
+    Exp::E
+end
+CFET4BfCPropagator{E<:Exponentiator}(B::LinearOperator,
+                                     f::Function,
+                                     C::LinearOperator,
+                                     Exp::E) =
+                                         CFET4BfCPropagator(B, f, C, 1, Exp)
+
+function call{E<:Exponentiator}(p::CFET4BfCPropagator{E},
+                                t::Real, τ::Real,
+                                v::StridedArray, w::StridedArray)
+    h1 = 37/66 - 400/957*sqrt(5/3)
+    h2 = -4/33
+    h3 = 37/66 + 400/957*sqrt(5/3)
+    h4 = -11/162
+    h5 = 92/81
+
+    ff1 = p.f(t + (1/2-sqrt(3/20))*τ)
+    ff2 = p.f(t + 1/2*τ)
+    ff3 = p.f(t + (1/2+sqrt(3/20))*τ)
+
+    f1 = h1*ff1 + h2*ff2 + h3*ff3
+    f2 = h4*ff1 + h5*ff2 + h4*ff3
+    f3 = h3*ff1 + h2*ff2 + h1*ff3
+
+    τ1 = 11/40*τ
+    τ2 = 9/20*τ
+
+    p.Exp(p.B + f3*p.C, p.a*τ1, v, w)
+    p.Exp(p.B + f2*p.C, p.a*τ2, w, w)
+    p.Exp(p.B + f1*p.C, p.a*τ1, w, w)
+end
+
+export MagnusPropagator, MidpointPropagator, CFET4BfCPropagator
